450f1adfb53f057baccd16dfa04a45f781a48509
Andrew Noblet <andrewbnoblet@gmail.com>
2021-10-12 13:13:38 -0400

    feat(wordpress-headless): update wordpress host

--- src/components/cxl-dashboard/WooCommerce.js
+++ src/components/cxl-dashboard/WooCommerce.js
@@ -2,7 +2,7 @@ import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
 export const WooCommerce = ({ version } = { version: "wc/v3" }) =>
     new WooCommerceRestApi({
-        url: "http://127.0.0.1:8080/",
+        url: "http://205e-108-14-111-165.ngrok.io",
         consumerKey: "ck_6671ea814aad12258d6035fc2369c37fd8b0b61b",
         consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
         version,
