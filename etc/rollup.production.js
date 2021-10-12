2811992da23c759fe6429faa0d481ff3e0640777
Andrew Noblet <andrewbnoblet@gmail.com>
2021-10-12 12:31:55 -0400

    fix(wordpress-headless): update rollup production configuration

--- etc/rollup.production.js
+++ etc/rollup.production.js
@@ -1,6 +1,9 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import minifyHTML from "rollup-plugin-minify-html-literals";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import { terser } from "rollup-plugin-terser";
 
 module.exports = {
@@ -11,6 +14,9 @@ module.exports = {
         sourcemap: true,
     },
     plugins: [
+        commonjs(),
+        json(),
+        nodePolyfills(),
         replace({
             "process.env.NODE_ENV": JSON.stringify("production"),
         }),
