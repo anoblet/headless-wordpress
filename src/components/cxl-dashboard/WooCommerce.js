ffedeecc6c34d876d5b1b1782e8eedb5022e59b4
Andrew Noblet <andrewbnoblet@gmail.com>
2021-10-12 15:17:03 -0400

    feat(wordpress-headless): set queryStringAuth to true

--- src/components/cxl-dashboard/WooCommerce.js
+++ src/components/cxl-dashboard/WooCommerce.js
@@ -5,5 +5,6 @@ export const WooCommerce = ({ version } = { version: "wc/v3" }) =>
         url: "https://7af8-108-14-111-165.ngrok.io",
         consumerKey: "ck_6671ea814aad12258d6035fc2369c37fd8b0b61b",
         consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
+        queryStringAuth: true,
         version,
     });
