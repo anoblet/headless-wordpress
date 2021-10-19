02cf8f3022ae1541b5c1afad701b352d3a9c8e46
Andrew Noblet <andrewbnoblet@gmail.com>
2021-10-18 20:19:14 -0400

    feat(headless-wordpress): save

--- src/components/cxl-dashboard/WooCommerce.js
+++ src/components/cxl-dashboard/WooCommerce.js
@@ -3,7 +3,7 @@ import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 export const WooCommerce = ({ version } = { version: "wc/v3" }) =>
     new WooCommerceRestApi({
         // url: "http://localhost:8080",
-        url: "https://f823-108-14-111-165.ngrok.io",
+        url: "https://375c-108-14-111-165.ngrok.io",
         consumerKey: "ck_6671ea814aad12258d6035fc2369c37fd8b0b61b",
         consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
         version,
