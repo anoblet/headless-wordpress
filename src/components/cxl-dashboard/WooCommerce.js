a596de179b07db850cbb69680650c0cc48b6154f
Andrew Noblet <andrewbnoblet@gmail.com>
2021-10-28 11:49:32 -0400

    refactor(headless-wordpress): save

--- src/components/cxl-dashboard/WooCommerce.js
+++ src/components/cxl-dashboard/WooCommerce.js
@@ -1,10 +1,13 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import { config } from "../../config";
 
-export const WooCommerce = ({ version } = { version: "wc/v3" }) =>
-    new WooCommerceRestApi({
-        // url: "http://localhost:8080",
-        url: "https://1365-108-14-111-165.ngrok.io",
-        consumerKey: "ck_6671ea814aad12258d6035fc2369c37fd8b0b61b",
-        consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
+export const WooCommerce = ({ version } = { version: "wc/v3" }) => {
+    const { url, consumerKey, consumerSecret } = config.wooCommerce;
+
+    return new WooCommerceRestApi({
+        url,
+        consumerKey,
+        consumerSecret,
         version,
     });
+};
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+    wooCommerce: {
+        // url: "http://localhost:8080",
+        url: "https://nervous-snake-11.loca.lt/",
+        consumerKey: "ck_6671ea814aad12258d6035fc2369c37fd8b0b61b",
+        consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
+    },
+    wordpress: {
+        url: "",
+    },
+};
