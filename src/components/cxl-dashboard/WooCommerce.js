a2dcb9d46fdc9dd6596bd6da4a2534f0d10e978d
Andrew Noblet <andrewbnoblet@gmail.com>
2021-10-28 16:22:31 -0400

    refactor(headless-wordpress): save

--- src/components/cxl-dashboard/WooCommerce.js
+++ src/components/cxl-dashboard/WooCommerce.js
@@ -5,6 +5,11 @@ export const WooCommerce = ({ version } = { version: "wc/v3" }) => {
     const { url, consumerKey, consumerSecret } = config.wooCommerce;
 
     return new WooCommerceRestApi({
+        axiosConfig: {
+            headers: {
+                "Bypass-Tunnel-Reminder": true,
+            },
+        },
         url,
         consumerKey,
         consumerSecret,
