aaef4d556ebd3dd5fbd6f4a5ca159730b2898c14
Andrew Noblet <andrewbnoblet@gmail.com>
2021-11-09 11:59:24 -0500

    refactor(headless-wordpress): save

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     wooCommerce: {
         // url: "http://localhost:8080",
-        url: "https://dull-dragonfly-34.loca.lt",
+        url: "https://angry-parrot-54.loca.lt",
         consumerKey: "ck_6671ea814aad12258d6035fc2369c37fd8b0b61b",
         consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
     },
