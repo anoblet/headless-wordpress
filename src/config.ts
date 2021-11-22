7cd8ca51199366fe1617967093ba82e3e95b2d97
Andrew Noblet <andrewbnoblet@gmail.com>
2021-11-22 10:45:28 -0500

    refactor(headless-wordpress): save

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,10 @@ export const config = {
     },
     title: "Customer Service Dashboard",
     wooCommerce: {
-        url: "https://green-wolverine-11.loca.lt",
         consumerKey: "ck_6671ea814aad12258d6035fc2369c37fd8b0b61b",
         consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
     },
     wordpress: {
-        url: "https://green-wolverine-11.loca.lt",
+        url: "https://empty-deer-54.loca.lt",
     },
 };
