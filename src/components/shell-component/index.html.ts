37eb5291b37886954080a0ee32ed476ca718465b
Andrew Noblet <andrewbnoblet@gmail.com>
2021-11-22 10:45:00 -0500

    refactor(headless-wordpress): save

--- src/components/shell-component/index.html.ts
+++ src/components/shell-component/index.html.ts
@@ -2,5 +2,5 @@ import { html } from "lit-element";
 import "../cxl-dashboard/Views/Dashboard/cxl-dashboard";
 
 export default function () {
-    return html` <cxl-dashboard></cxl-dashboard> `;
+    return html`<cxl-dashboard></cxl-dashboard>`;
 }
