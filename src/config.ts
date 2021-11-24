92f6ebf84c99983fbf820d2ad954ac7cac6dc76e
Andrew Noblet <andrewbnoblet@gmail.com>
2021-11-24 11:46:37 -0500

    refactor(headless-wordpress): save

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,6 @@ export const config = {
         consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
     },
     wordpress: {
-        url: "https://purple-mole-50.loca.lt",
+        url: "https://hot-wombat-99.loca.lt",
     },
 };
