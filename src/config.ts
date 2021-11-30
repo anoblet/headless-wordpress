38797a84d5d145bf0934276246544e125431d89b
Andrew Noblet <andrewbnoblet@gmail.com>
2021-11-30 09:34:20 -0500

    refactor(headless-wordpress): save

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ export const config = {
     },
     title: "Customer Service Dashboard",
     wooCommerce: {
-        consumerKey: "ck_6671ea814aad12258d6035fc2369c37fd8b0b61b",
-        consumerSecret: "cs_d650eefab1f2d4ec819a3797b735f20863ed701c",
+        consumerKey: "XX_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        consumerSecret: "XX_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     },
     wordpress: {
         url: "https://nasty-skunk-26.loca.lt",
